b1dcd299c3638a9b3ed603c2f7cc5f6d9c674315
UNT48 <buchap@masterpiece-imac.local>
2018-06-20 07:43:12 +0900

    Initial

---
+++ Assets/App/Scripts/PhotoSet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Crosstales.FB;
+
+public class PhotoSet : MonoBehaviour {
+
+	private void OnCollisionEnter(Collision coll){
+		Debug.Log("CollisionEnter");
+	}
+
+	private void OnTriggerEnter(Collider coll){
+		Debug.Log("Trigger Enter");
+	}
+
+	public void OnRayHit(){
+		StartCoroutine(getFile());
+	}
+	
+	private IEnumerator getFile(){
+		Cursor.lockState = CursorLockMode.None;
+		Cursor.visible = true;
+		yield return new WaitForEndOfFrame();
+		string filePath = FileBrowser.OpenSingleFile("Open Files", "", "");
+		yield return new WaitForFixedUpdate();
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+		yield return null;
+	}
+}
---
+++ Assets/App/Scripts/WorkSetter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkSetter : MonoBehaviour {
+	private float rayLength = 500f;
+	public LayerMask rayExclusionLayers;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		TouchInfo info = AppUtil.GetTouch();
+		if(info == TouchInfo.Began){
+			Ray ray = Camera.main.ScreenPointToRay(AppUtil.GetTouchPosition());
+			RaycastHit hit;
+
+			if(Physics.Raycast(ray, out hit, rayLength, ~rayExclusionLayers)){
+				Debug.Log("hit object name"+hit.transform.gameObject.name);
+
+				if(hit.transform.GetComponent<PhotoSet>() != null){
+					hit.transform.GetComponent<PhotoSet>().OnRayHit();
+				}
+			}
+		}
+	}
+
+	
+}
